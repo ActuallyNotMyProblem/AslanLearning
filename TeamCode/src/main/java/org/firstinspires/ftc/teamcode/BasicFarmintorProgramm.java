b3cdcd8678c2401a61d38c1dddc88731d05810e1
Barnyard22993 <barnyard22993@gmail.com>
2026-05-11 17:48:50 +0300

    11/05

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicFarmintorProgramm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicFarmintorProgramm.java
@@ -7,14 +7,25 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+/*
+Tasks:
+- drivetrain
+- intake
+- transfer
+- kickstand
+- gate
+- hood
+ */
+
 @TeleOp(name="Basic: Farmintor Programm", group="Linear OpMode")
 @Disabled
 public class BasicFarmintorProgramm extends LinearOpMode {
-    private ElapsedTime running = new ElapsedTime();
+    private ElapsedTime running = new ElapsedTime(); //variables
     private DcMotor front_left = null;
     private DcMotor front_right = null;
     private DcMotor back_left = null;
     private DcMotor back_right = null;
+
 
 
     @Override
@@ -32,9 +43,43 @@ public class BasicFarmintorProgramm extends LinearOpMode {
 
         waitForStart();
 
+        while (opModeIsActive()) {
+            double max;
 
+            double axial = -gamepad1.left_stick_y; // axial - forward/back (left stick Y), "-"
+            double lateral = gamepad1.left_stick_x;// lateral - the sideways movement (left, X)
+            double pitch = gamepad1.right_stick_x;// yaw - the rotation (right, x)
+
+            double front_right_power = axial + lateral + pitch;
+            double front_left_power = axial - lateral - pitch;
+            double back_left_power = axial - lateral + pitch;
+            double back_right_power = axial + lateral - pitch;
+
+            max = Math.max(Math.max(Math.max(Math.abs(front_left_power), Math.abs(front_left_power)), Math.abs(back_left_power)),Math.abs(back_right_power));
+
+            if (max>1) {
+                front_left_power /= max;
+                front_right_power /= max;
+                back_left_power /= max;
+                back_right_power /= max;
+            }
+
+            front_left.setPower(front_left_power);
+            front_right.setPower(front_right_power);
+            back_left.setPower(back_left_power);
+            back_right.setPower(back_right_power);
+
+            telemetry.addData("status: ", "time" + running.toString());
+            telemetry.update();
+
+
+
+
+
+        }
 
 
 
     }
+
 }
